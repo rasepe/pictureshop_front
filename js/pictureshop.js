f25b195b03c38ece04817499caa61d3476ed52a5
Rafel <rasepe@gmail.com>
2020-02-14 13:03:24 +0100

    1402

--- js/pictureshop.js
+++ js/pictureshop.js
@@ -1,5 +1,26 @@
 
 var arrayShops = new Array();
+var arrayShopIds = new Array();
+var counter = 0;
+
+function sleep(ms) {
+	  return new Promise(resolve => setTimeout(resolve, ms));
+	}
+
+	async function demo() {
+	  console.log('Taking a break...');
+	  await sleep(2000);
+	  console.log('Two seconds later, showing sleep in a loop...');
+
+	  // Sleep in loop
+	  for (let i = 0; i < 5; i++) {
+	    if (i === 3)
+	      await sleep(2000);
+	    console.log(i);
+	  }
+	}
+
+	
 
 function loadIds(shopId) {
 	var constructedUrl = "http://localhost:8080/shops/";
@@ -35,8 +56,8 @@ function addNewShop() {
 	}
 
 
-	//console.log(newEmployee);
-
+	console.log(newShop);
+	demo();
 
 	$.ajax({
 		type: "POST",
@@ -44,7 +65,7 @@ function addNewShop() {
 		url: "http://localhost:8080/shops",
 		data: JSON.stringify(newShop),    
 		success: function(data) {
-
+			console.log("success");
 		},
 		error: function(){
 			alert("json not found");
@@ -74,6 +95,104 @@ function seeAllShops() {
 
 }
 
+
+function listPictures() {
+	
+	var currentShopId = document.getElementById("shopId")[document.getElementById("shopId").selectedIndex].value;
+
+	var constructedUrl = "http://localhost:8080/shops/" + currentShopId + "/pictures";
+
+	console.log(constructedUrl);
+	//var result;
+	$.ajax({
+		type: "GET",
+		url: constructedUrl,
+		success: function(data) {
+
+			print(data, "pictureList");
+
+		},
+		error: function(){
+			alert("json not found");
+		}
+	});
+
+
+}
+
+function listPictures2() {
+	
+	var currentShopId = document.getElementById("shopId")[document.getElementById("shopId").selectedIndex].value;
+
+	var constructedUrl = "http://localhost:8080/shops/" + currentShopId + "/pictures";
+
+	console.log(constructedUrl);
+	//var result;
+	$.ajax({
+		type: "GET",
+		url: constructedUrl,
+		success: function(data) {
+
+			getIds(data)
+
+		},
+		error: function(){
+			alert("json not found");
+		}
+	});
+
+
+}
+
+function getIds(data) {
+	
+	for (var i=0; i<data.content.length; i++) {
+	
+	arrayShopIds.push(data.content[i].id);
+		
+	}
+	//return arrayShopIds;
+}
+
+
+function deletePictures() {
+	
+	listPictures2();
+	if (counter == 0) {
+		document.getElementById("confirmation").innerHTML="Please click again to confirm deletion";
+		counter++;
+	} else {
+		document.getElementById("confirmation").innerHTML="Deletion confirmed";
+	}
+	//listPictures2();
+	console.log(arrayShopIds);
+	var currentShopId = document.getElementById("shopId")[document.getElementById("shopId").selectedIndex].value;
+	
+	for (var i=0; i<arrayShopIds.length; i++) {
+		var constructedUrl = "http://localhost:8080/shops/" + currentShopId + "/pictures/" + arrayShopIds[i];
+		// /shops/{shopId}/pictures/{pictureId}
+		console.log(constructedUrl);
+		$.ajax({
+			type: "DELETE",
+			contentType: "application/json",
+			url: constructedUrl,
+			data: arrayShopIds[i], 
+			success: function(data) {
+				console.log("success");
+				console.log("esborrem" + i);
+
+			},
+			error: function(){
+				alert("json not found");
+			}
+		});
+		
+		
+	}
+	
+
+
+}
 
 function getCurrentShop() {
 	
@@ -129,11 +248,15 @@ function manage(objects,shopId) {
 
 function addNewPicture() {
 
+	
+	
 	var newPictureAuthor = document.getElementById("pictureAuthor").value;
 	var newPictureName = document.getElementById("pictureName").value;
 	var newPicturePrice = parseFloat(document.getElementById("picturePrice").value);
     var currentShopId = document.getElementById("shopId")[document.getElementById("shopId").selectedIndex].value;
     var currentShop = arrayShops[currentShopId-1];
+    
+    cleanFields();
    // getShopById(currentShopId);
     //getCurrentShop();
     //console.log(currentShop);
@@ -178,13 +301,19 @@ function addNewPicture() {
 		url: constructedURL,
 		data: JSON.stringify(newPicture),  //params,  // '{"id":'+currentShopId+'}' + JSON.stringify(newPicture), 
 		success: function(data) {
-
+			console.log("success");
 		},
 		error: function(){
 			alert("json not found");
 		}
 	});
 
+}
+
+function cleanFields(){
+	document.getElementById("pictureAuthor").value="";
+	document.getElementById("pictureName").value="";
+	document.getElementById("picturePrice").value="";
 }
 
 function print(objects, id) {
